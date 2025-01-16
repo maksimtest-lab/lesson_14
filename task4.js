b10efcd7768a681d0feb106e8f4c961027656f2f
Maksim Alipau <maxanotest@gmail.com>
2025-01-16 21:07:03 +0300

    feat: Add a task 4

---
+++ task4.js
@@ -0,0 +1,16 @@
+// --- 1
+let number = "123";
+let numberValue = Number(number);
+let square = numberValue ** 2;
+let remainder = numberValue % 7;
+
+console.log(`Квадрат: ${square}`);
+console.log(`Остаток от деления на 7: ${remainder}`);
+
+// --- 2
+let num1 = prompt('Введите первое число');
+let num2 = prompt('Введите второе число');
+
+let sum = Number(num1) + Number(num2);
+
+console.log(`Сумма: ${sum}`);
