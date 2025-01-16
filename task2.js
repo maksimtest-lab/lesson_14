a815954c5e8e92a0188c971d419b1aea61031f99
Maksim Alipau <maxanotest@gmail.com>
2025-01-16 20:52:30 +0300

    feat: Add a task 2

---
+++ task2.js
@@ -0,0 +1,8 @@
+let value = 42;
+console.log(typeof value); // number
+
+value = "Hello, world!";
+console.log(typeof value); // string
+
+let userInput = prompt("Введите значение:");
+console.log(typeof userInput);
