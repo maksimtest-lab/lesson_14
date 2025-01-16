90fe9adeaab848fe1275607083ba25c83aef8fa4
Maksim Alipau <maxanotest@gmail.com>
2025-01-16 20:47:28 +0300

    Init.

---
+++ task1.js
@@ -0,0 +1,12 @@
+let name = 'Максим';
+let age = 27;
+let city = 'Минск';
+
+console.log(`Привет, меня зовут ${name}. Мне ${age} лет, и я живу в городе ${city}.`);
+
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+
+console.log(a, b); // 20 10
